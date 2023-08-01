a70d7e04cb3fe8494c94bd85fa81e7dc98d81b7a
karolinasocha <karolina.zofia.socha@gmail.com>
2023-08-01 17:15:57 +0200

    added new plot Figure3E awake anesthesia density plots stats

---
+++ functions/example_select_with_weight_probability.m
@@ -0,0 +1,7 @@
+
+%% select one session with weight probability
+dataArray = [10, 20, 30, 40]; % Example array
+weights=(1/length(dataArray))*(ones(1,numel(dataArray)))
+
+numSamples = 1; % number of session selected
+selectedIndices = randsample(numel(dataArray), numSamples, true, weights);
