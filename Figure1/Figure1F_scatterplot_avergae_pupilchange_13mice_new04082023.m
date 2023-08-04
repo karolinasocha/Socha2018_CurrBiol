3f304e09d54dfc799e491fba70a673ac54544943
karolinasocha <karolina.zofia.socha@gmail.com>
2023-08-04 14:13:32 +0200

    added plot SupplFigure1C delta velocity witm mean SEM and CI

--- Figure1/Figure1F_scatterplot_avergae_pupilchange_13mice_new04082023.m
+++ Figure1/Figure1F_scatterplot_avergae_pupilchange_13mice_new04082023.m
@@ -1,4 +1,4 @@
-%%
+%% Figure 1F edited on 04 August 2023
 % statistical test for single trial
 % plot heatmaps with statistical significance
 
@@ -125,6 +125,15 @@ plot([-100 100], [-100 100],'--k');
 ylim([-10, 70])
 xlim([-10, 40])
 
+ylabel('Pupil size change (%) NASAL', 'FontSize',18,'Color','k');
+xlabel('Pupil size change (%) TEMPORAL','FontSize',18,'Color','k');
+set(gca,'tickdir','out','fontsize',14,'ticklength',get(gca,'ticklength')*4);
+box off
+
+set(gcf,'paperunits','centimeters','papersize' ,[21,29.7],'color','w','paperposition',[0,0,21,29.7],'inverthardcopy','off');
+filepathanalysis=['G:\mousebox\code\mouselab\users\karolina\FiguresPaper2023\Figure1\scripts\'];
+print(gcf,'-dpdf',[filepathanalysis, 'Figure1F_raw_diam_relative_delta_pupil_SEM_mice.pdf']);
+
 %% 
 %% WITH CI
 colors=jet(13);
@@ -168,65 +177,5 @@ set(gcf,'paperunits','centimeters','papersize' ,[21,29.7],'color','w','paperposi
 filepathanalysis=['G:\mousebox\code\mouselab\users\karolina\FiguresPaper2023\Figure1\scripts\'];
 print(gcf,'-dpdf',[filepathanalysis, 'Figure1F_raw_diam_relative_delta_pupil_CI_mice.pdf']);
 
-%%
-% plot
-% Create a scatter plot with error bars
-figure
-x = 1:size(data_example_mean, 1);
-errorbar(x, mean_values, mean_values - lower_ci, upper_ci - mean_values, 'o', 'MarkerFaceColor', 'b', 'MarkerEdgeColor', 'b', 'LineWidth', 1.5);
 
    
-%% scatter plot COLOR CODED ANIMAL
-
-colors=jet(13);
-
-order_nasal=[11,12,1,2,3,4];
-order_temporal=[5,6,7,8,9,10];
-
-av_delta_animals=cell2mat(pupil_animals_cell')
-figure
-for iAn=1:length(pupil_animals_cell)
-    clear data_temporal
-    clear data_nasal
-    
-    data_nasal=pupil_animals_cell{iAn}(order_nasal)
-    data_temporal=pupil_animals_cell{iAn}(order_temporal)
-    hold on
-%   plot(100*data_temporal(:),100*data_nasal(:),'o','MarkerSize', 6,'MarkerFaceColor', [0.7,0.7,0.7],'MarkerEdgeColor', [0.5, 0.5, 0.5]); %colors_scatter(iSess,:)
-    plot(100*data_temporal(:),100*data_nasal(:),'o','MarkerSize', 6,'MarkerFaceColor', colors(iAn,:),'MarkerEdgeColor', [0.5, 0.5, 0.5]); %colors_scatter(iSess,:)
-
-    data_nasal_all{iAn}=nanmean(data_nasal);
-    data_temporal_all{iAn}=nanmean(data_temporal);
-end
-
-hold on
-plot([-100 100], [-100 100],'--k');
-
-ylim([-10, 70])
-xlim([-10, 40])
-
-for_test_nasal_data=cell2mat(data_nasal_all');
-for_test_temporal_data=cell2mat(data_temporal_all');
-
-% for plots calculate average point 
-mean_all_nasal=100*nanmean(for_test_nasal_data(:),1);
-sem_all_nasal=100*nanstd(for_test_nasal_data(:),1)./sqrt(size(for_test_nasal_data, 1));
-
-mean_all_temporal=100*nanmean(for_test_temporal_data(:),1);
-sem_all_temporal=100*nanstd(for_test_temporal_data(:),1)./sqrt(size(for_test_temporal_data, 1));
-
-
-[pval, h0, stats]=signrank(for_test_nasal_data(:), for_test_temporal_data(:),'Tail','right');
-pval
-
-hold on;
-plot(mean_all_temporal, mean_all_nasal, 'o', 'MarkerSize', 6, 'MarkerFaceColor', 'k', 'MarkerEdgeColor', 'k');
-
-ylabel('Pupil size change (%) NASAL', 'FontSize',18,'Color','k');
-xlabel('Pupil size change (%) TEMPORAL','FontSize',18,'Color','k');
-set(gca,'tickdir','out','fontsize',14,'ticklength',get(gca,'ticklength')*4);
-box off
-
-set(gcf,'paperunits','centimeters','papersize' ,[21,29.7],'color','w','paperposition',[0,0,21,29.7],'inverthardcopy','off');
-filepathanalysis=['G:\mousebox\code\mouselab\users\karolina\FiguresPaper2023\Figure1\scripts\'];
-%print(gcf,'-dpdf',[filepathanalysis, 'average_raw_diam_relative_delta_pupil_SEM_sessions.pdf']);
